965a3d10460d6b50d6e3a8297b4721f9ebc79acf
HaNaK0 <hampus@hampushuledal.com>
2023-09-25 20:23:47 +0200

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use bevy::{prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window { 
-                    resolution: (640.0, 480.0).into(), 
-                    resizable: false,
-                    title: "rustiant".to_string(), 
-                    ..default()
-                }),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (640.0, 480.0).into(),
+                resizable: false,
+                title: "rustiant".to_string(),
                 ..default()
-            })
-        )
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .add_systems(Update, movement_system)
         .run();
@@ -21,33 +19,32 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
-        sprite: Sprite { 
+        sprite: Sprite {
             custom_size: (Some(Vec2::new(100.0, 100.0))),
             ..default()
         },
-        texture : asset_server.load("bevy-icon.png"),
+        texture: asset_server.load("bevy-icon.png"),
         ..default()
     });
 }
 
 fn movement_system(
-    mut transforms: Query<&mut Transform, With<Sprite>>, 
+    mut transforms: Query<&mut Transform, With<Sprite>>,
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
-)
-    {
-        for mut transform in &mut transforms {
-            if keyboard_input.pressed(KeyCode::A) {
-                transform.translation.x -= 100.0 * time.delta_seconds();
-            }
-            if keyboard_input.pressed(KeyCode::D) {
-                transform.translation.x += 100.0 * time.delta_seconds();
-            }
-            if keyboard_input.pressed(KeyCode::W) {
-                transform.translation.y += 100.0 * time.delta_seconds();
-            }
-            if keyboard_input.pressed(KeyCode::S) {
-                transform.translation.y -= 100.0 * time.delta_seconds();
-            }
+) {
+    for mut transform in &mut transforms {
+        if keyboard_input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
         }
+        if keyboard_input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+    }
 }
